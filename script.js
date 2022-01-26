b2569359f81b79e79a19326479b82ee08012dc6f
DiogoSales07 <dsales289@poli.ufrj.br>
2022-01-26 17:19:31 -0300

    Estudando arrays

---
+++ script.js
@@ -0,0 +1,22 @@
+let foods = ["frango ensopado","bolo","hamburguer","churrasco","macarronada"]
+console.log(foods)
+
+let colors=["azul","amarelo"]
+let add = colors.unshift("vermelho")
+console.log(colors)
+
+let animals=["leao","elefante","girafa"]
+let remove = animals.pop()
+console.log(animals)
+
+let tecs = ["celular","tv","radio"]
+let adds = tecs.push("pc","tablets")
+console.log(tecs)
+
+let countries = ["brasil","Japao","França"]
+let removes = countries.shift()
+console.log(countries)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+let plus = numbers.sort()
+console.log(numbers)
